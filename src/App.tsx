de61b1f7f986df652f7bd039ab373b5d03d2d0cd
olegbucolo <olegbucolo97@gmail.com>
2026-01-04 17:21:53 +0100

    Create Header component (shared)

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { HomePage } from './home/HomePage'
 import { AboutPage } from './about/AboutPage'
 import { JobsPage } from './jobs/JobsPage'
 import { Routes, Route } from 'react-router-dom';
+import { Header } from './components/Header';
 
 function App() {
 
   return (
     <>
+      <Header />
       <Routes>
         <Route index element={<HomePage />} />
         <Route path={"jobs"} element={<JobsPage />} />
---
+++ src/components/Header.tsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom';
+import './Header.css';
+
+export function Header() {
+    return (
+        <header className="header-main">
+            <nav className="navbar">
+                <ul className="ul-l">
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/jobs">Jobs</NavLink></li>
+                    <li><NavLink to="/about">About</NavLink></li>
+                </ul>
+                <ul className="ul-r">
+                    <li><NavLink to="/login">Login</NavLink></li>
+                    <li><NavLink to="/subscribe">Subscribe</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
