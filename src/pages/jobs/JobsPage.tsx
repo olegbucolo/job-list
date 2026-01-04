9500254dc914517051f29db5008ca1c09e49975d
olegbucolo <olegbucolo97@gmail.com>
2026-01-05 00:29:46 +0100

    Added the .json file to localStorate for the backend, made the setup of the future components in JobsPage.tsx

--- src/pages/jobs/JobsPage.tsx
+++ src/pages/jobs/JobsPage.tsx
@@ -1,10 +1,26 @@
+import type {Job} from '../../types/job';
+import {useState, useEffect} from 'react';
+import rawJobsData from '../data/jobs.json'
+
 export function JobsPage() {
+
+    const [jobs, setJobs] = useState<Job[]>(() => {
+
+        const stored = localStorage.getItem('jobs');
+        return stored ? (JSON.parse(stored) as Job[]) : rawJobsData as Job[]
+    })
+
+    useEffect(() => {
+        localStorage.setItem('jobs', JSON.stringify(jobs));
+    })
+
     return (
         <section>
             <div>jobs page</div>
             {/* search thing? */}
             {/* filters thing*/}
             {/* job lists */}
+            
         </section>
     )
 }
