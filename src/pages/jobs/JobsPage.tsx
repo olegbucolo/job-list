4dea446a4d1ad10040879651bc58983ac27943ca
olegbucolo <olegbucolo97@gmail.com>
2026-01-05 18:23:12 +0100

    Create JobDetailsPage component, add 'description' to the jobs.json component

--- src/pages/jobs/JobsPage.tsx
+++ src/pages/jobs/JobsPage.tsx
@@ -6,9 +6,12 @@ import { JobList } from './JobList';
 export function JobsPage() {
 
     const [jobs, setJobs] = useState<Job[]>(() => {
-
         const stored = localStorage.getItem('jobs');
         return stored ? (JSON.parse(stored) as Job[]) : rawJobsData as Job[]
+    })
+
+    useEffect(() => {
+        localStorage.setItem('jobs', JSON.stringify(jobs));
     })
 
     const applyForJob = (id: number) => {
@@ -17,19 +20,15 @@ export function JobsPage() {
                 j.id === id ? { ...j, applied: !j.applied } : j
             )
         )
-}
+    }
 
-useEffect(() => {
-    localStorage.setItem('jobs', JSON.stringify(jobs));
-})
+    return (
+        <section>
+            {/* search thing? */}
+            {/* filters thing*/}
+            {/* job lists */}
+            <JobList jobs={jobs} onJobApply={applyForJob} />
 
-return (
-    <section>
-        {/* search thing? */}
-        {/* filters thing*/}
-        {/* job lists */}
-        <JobList jobs={jobs} onJobApply={applyForJob} />
-
-    </section>
-)
+        </section>
+    )
 }
